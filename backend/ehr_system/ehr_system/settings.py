61d31abc3f7f4d7a58620d2e346c17bac6772d30
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-02-06 00:51:26 +0530

    move secret key to env

--- backend/ehr_system/ehr_system/settings.py
+++ backend/ehr_system/ehr_system/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@0+nk0#9e=68@okr=-oamnf8h6u_(wov+hw77ue9767hp-#l42'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
