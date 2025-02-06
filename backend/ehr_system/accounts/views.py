c7a39154264724c127736f93df1124691c50b2ee
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-02-07 02:07:22 +0530

    create login, logout and doctor registration views

--- backend/ehr_system/accounts/views.py
+++ backend/ehr_system/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate, login, logout
+from .serializers import UserSerializer,DoctorSerializer
 # Create your views here.
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+class LogoutView(APIView):
+    def get(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
+    
+class DoctorRegistrationView(generics.CreateAPIView):
+    serializer_class = DoctorSerializer
