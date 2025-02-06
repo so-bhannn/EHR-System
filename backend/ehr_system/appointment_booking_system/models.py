5b015385b45ca6faac6845dfb7d33d8ced51593b
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-02-06 22:32:25 +0530

    used uuid to generate unique ids for doctors

--- backend/ehr_system/appointment_booking_system/models.py
+++ backend/ehr_system/appointment_booking_system/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from api.models import Patient
+import uuid
 
 # Create your models here.
 
 class Doctor(models.Model):
     name=models.CharField(max_length=30)
+    doctor_id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     speciality=models.CharField(max_length=30)
     contact=models.IntegerField()
 
