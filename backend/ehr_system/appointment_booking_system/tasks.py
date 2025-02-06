3e717715204701c531e473b600679eb4293dff3d
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-02-06 23:27:21 +0530

    modify tasks for booking, cancellation, reschedule & complete appointments

--- backend/ehr_system/appointment_booking_system/tasks.py
+++ backend/ehr_system/appointment_booking_system/tasks.py
@@ -3,12 +3,25 @@ from django.core.mail import send_mail
 from .models import Appointment
 
 @shared_task
-def send_booking_confirmation(appointment_id):
-    appointment = Appointment.objects.get(id=appointment_id)
-    subject = 'Appointment Confirmation'
-    message = f'''
+def send_notification(appointment_id):
+    appointment=Appointment.objects.get(id=appointment_id)
+    status=appointment.status
+    if status=='BOOKED':
+        sub_msg='Confirmation'
+        msg='confirmed'
+    elif status=='CANCELLED':
+        sub_msg='Cancellation'
+        msg='cancelled'
+    elif status=='RESCHEDULED':
+        sub_msg='Reschedule'
+        msg='rescheduled'
+    else:
+        sub_msg='Completion'
+        msg='completed'
+    subject=f'Appointment {sub_msg}'
+    message=f'''
         Your appointment with Dr. {appointment.doctor.name}
-        on {appointment.slot.start_time} is confirmed.
+        on {appointment.slot.start_time} has been {msg}.
     '''
-    recipient_list = [appointment.patient.contact]
+    recipient_list=[appointment.patient.contact]
     send_mail(subject, message, 'admin@ehr-system.com', recipient_list)
