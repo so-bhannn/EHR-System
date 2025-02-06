0d63bd7142614a2af9870e329ae22f7e4826c541
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-02-07 00:23:47 +0530

    create routes for booking, cancelling, rescheduling and checking slot availability

---
+++ backend/ehr_system/appointment_booking_system/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('slots/<str:doctor_id>', list_available_slots, name='slots_available'),
+    path('book/', book_appointment, name='book_appointment'),
+    path('reschedule/<str:appointment_id>', reschedule_appointment, name='reschedule_appointment'),
+    path('cancel/<str:appointment_id>', cancel_appointment, name='cancel_appointment'),
+]
