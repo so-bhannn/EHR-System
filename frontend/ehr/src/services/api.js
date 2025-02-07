4fee147c012b595bba24d2fb1e111c5e7dfc0dc5
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-02-07 11:36:30 +0530

    register patient logic

---
+++ frontend/ehr/src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:8000';
+
+export const registerPatient = async (data) => {
+  const response = await axios.post(`${API_URL}/patients/register`, data);
+  return response.data;
+};
